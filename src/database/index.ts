bbb86d19e9b70bf04543ac59fbc3e8dd97a1dd95
Guilherme-Humberto <gbreu@outlook.com>
2020-10-25 21:13:09 -0300

    Configuando conexão com banco de dados

---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+import { connect, Promise } from 'mongoose'
+
+export const connetion = connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(() => console.log("Conectado ao banco de dados"))
+.catch((err) => console.log(`Erro ao conectar ao banco de dados ${err}`))
